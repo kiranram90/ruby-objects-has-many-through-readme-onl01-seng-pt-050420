88a824aaf5300f08d23f6546f065513a7aee2507
kiran ram <kram@lions.molloy.edu>
2020-06-11 03:49:26 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,17 @@
 class Waiter
+  
+  @@all = []
+
+attr_accessor :name, :experience
+  
+  def initialize(name, experience)
+    @name = name
+    @experience = experience
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
 
 end
